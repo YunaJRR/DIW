3ea94c35d26f4c006fb86dcc283f0424369f30a5
Jaime <jaimerocharodriguez04@gmail.com>
2025-01-10 14:18:04 +0100

    Scrollbar

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	const btnCambiaFondoSeccion1 = document.getElementById('btnCambiaFondoSeccion1');
 	const btnContadorClicks  = document.getElementById('btnContadorClicks');
 	const textoDinamico = document.getElementById('textoDinamico');
+	const btnTamañoTexto = document.getElementById('btnTamañoTexto');
+	const btnCambiarTema = document.getElementById('btnCambiarTema');
+	const progresoScroll = document.getElementById("progresoScroll");
+	const altura = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 	let fecha = new Date();
 	let hora = fecha.getHours();
 	let clicks = 0;
+	let textoTamañoDefault = true;
 	
 	if(hora>6 && hora<12){
 		textoDinamico.innerHTML = 'Buenos días';
@@ -20,8 +25,25 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	btnContadorClicks.onclick = function() {
 		contarClicks();
 	}
+	btnTamañoTexto.onclick = function(){
+		cambiarTamañoTexto(textoTamañoDefault);
+		if (textoTamañoDefault){
+			textoTamañoDefault = false;
+		}else{
+			textoTamañoDefault = true;
+		}
+	}
+	btnCambiarTema.onclick = function(){
+		cambiarTema();
+	}
+	window.addEventListener("scroll", () => {
+		const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+		progresoScroll.style.width = `${(scrollTop / altura) * 100}%`;
+	});
 
 	function cambiaFondo(selector) {
+		let colorActual;
+		let colorAleatorio;
 		const colores = [
 			'lightblue',
 			'lightcoral',
@@ -37,11 +59,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
 			'lightsteelblue',
 			'lightyellow'
 		];
-		let colorAleatorio = colores[Math.floor(Math.random()*colores.length)]
+		
+		while (colorActual === colorAleatorio){
+			colorAleatorio = colores[Math.floor(Math.random()*colores.length)];
+		}
+		colorActual = colorAleatorio;
 		const seccion = document.querySelector(selector);
 		if (seccion) { 
 				seccion.style.background = colorAleatorio;
-				seccion.style.transition = 'all 1s';
+				seccion.style.transition = 'background .3s';
 		}
 	}
 
@@ -50,6 +76,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
 		document.getElementById('numeroClicks').innerHTML = 'Numero de clicks: ' + clicks;
 	}
 
-	
+	function cambiarTamañoTexto(tamañoDefault) {
+		const body = document.body;
+		const currentFontSize = window.getComputedStyle(body).fontSize;
+		const currentSize = parseFloat(currentFontSize);
+		if (tamañoDefault){
+			body.style.fontSize = (currentSize * 1.2) + 'px'; 
+		}else{
+			body.style.fontSize = (currentSize / 1.2) + 'px'; 
+		}
+		
+	}
+	function cambiarTema (){
+		const body = document.body;
+		body.classList.toggle("modo-oscuro");
+		body.style.transition = 'all .3s'
+		console.log("hey");
+		
+	}
 
 });
